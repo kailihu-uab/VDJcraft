da5a0f4e312d0696152ffff88e10f1b40b1d2219
kailihu-uab <135654382+kailihu-uab@users.noreply.github.com>
2024-03-07 09:17:00 -0600

    Update error_correction.py

--- error_correction.py
+++ error_correction.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 
-#file1_path = 'HG0268/vdjc_original.csv'
-
-#df1 = pd.read_csv(file1_path, sep='\t', header=0) #names=['query', 'chr','position', 'v_gene', 'd_gene', 'j_gene','c_gene'])
 
 
 def error_correction(df):
@@ -38,23 +35,4 @@ def error_correction(df):
         df['position'] = df['position'].astype(int)
     return df
 
-#with open('HG0268/vdjc_original.csv','r') as vdjc, open('HG0268/table.csv','r') as T:
-#        df_vdjc = pd.read_csv(vdjc, sep='\t', header=0)
-
-#        df_pos = pd.read_csv(T, sep='\t', header=0)
-
-#        pos_vdjc = pd.merge(df_pos, df_vdjc, on = 'Query', how='inner')
-
-#        pos_vdjc.to_csv('0268pos.txt', sep='\t', index=False)
-
-#        corrected_df = error_correction(pos_vdjc)
-
-    #    corrected_df['position'] = corrected_df['position'].astype(int)
-#        corrected_df.to_csv('0268correct.txt', sep='\t', index=False)
-# Apply the function
-#result_df = error_correction(df1)
-#result_df['position'] = result_df['position'].astype(int)
-#result_df.to_csv('2106correct.txt', sep='\t', index=False)
-# Print the result
-#print(result_df)
 
